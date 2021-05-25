c5f0e23b728e60caebe50781e09df04f81a929ca
Nitro01010 <crgautam2020@gmail.com>
2021-05-24 20:35:32 -0700

    i finally learned how to use atom to commit changes to my github repo :D

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,10 @@ console.log("Hello, world!");
 
 /* This is an infinite loop. It gives you 1 alert per ms.*/
 
-setTimeout(function(){ alert("Press the Hello Button, and press and hold the Escape Key"); }, 5000);
+// setTimeout(function(){ alert("Press the Hello Button, and press and hold the Escape Key"); }, 5000);
 
 function hello() {
   setInterval(function(){ alert("IM HAVING A SEIZURE HELP!!!!!!!!!!!"); }, 1);
-  
+
 
 }
