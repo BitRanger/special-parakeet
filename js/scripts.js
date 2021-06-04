a46a20396b3b8e3db7c4426d3209df750021eeb2
Nitro01010 <crgautam2020@gmail.com>
2021-06-04 09:59:28 -0700

    sdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,42 @@
-/* Hello World */
-
-console.log("Hello, world!");
-
-/* This is an infinite loop. It gives you 1 alert per ms.*/
-
 // setTimeout(function(){ alert("Press the Hello Button, and press and hold the Escape Key"); }, 5000);
 
-function hello() {
-  setInterval(function(){ alert("IM HAVING A SEIZURE HELP!!!!!!!!!!!"); }, 1);
+/*function hello() {
+  setInterval(function(){ prompt("IM HAVING A SEIZURE HELP!!!!!!!!!!!"); }, 1);
 
+}
 
+let x = 1;\\
+
+if(x = 1){
+	alert("Indu");
+	x ++;
+}
+if(x=2){
+	alert("Is");
+	x ++;
+}
+if(x=3) {
+	alert("Cool")
+
+}
+ */
+let welcome = "Hello, ";
+let username = 123/*prompt("What is you name?");*/
+if (username == null) {
+	document.getElementById("special").innerHTML = welcome + "Anonymous";
+} else {
+	document.getElementById("special").innerHTML = welcome + username;
+}
+
+function save() {
+	document.body.contentEditable = "false";
+	document.designMode = "off";
+	document.getElementById("#seizure-button").setAttribute("onclick", "edit()");
+	document.getElementById("#titlestuff:hover").style.color = "red";
+}
+
+function edit() {
+	document.body.contentEditable = "true";
+	document.designMode = "on";
+	document.getElementById("seizure-button").setAttribute("onclick", "save()");
 }
