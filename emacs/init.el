0c571157c768f5b45d403a0c8398ff48698efcb7
Nitro01010 <80020708+Nitro01010@users.noreply.github.com>
2023-07-17 14:17:18 -0700

    Add files via upload

---
+++ emacs/init.el
@@ -0,0 +1,20 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(catppuccin))
+ '(custom-safe-themes
+   '("61526419f6ffee91cae16a75bfc0f39f7e9621280cc405edeae15699091e7c73" default))
+ '(delete-selection-mode nil)
+ '(warning-suppress-log-types '((comp))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
